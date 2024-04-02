4b92a4d9deca0d1ec09bdde76b409bf512b75c6d
Mehmet Emin BAŞOĞLU <mehmeteminbasoglu@hotmail.com>
2024-04-02 15:22:54 +0300

    feat: publish longitudinal can message

--- leodrive_gatevcu_adapter/include/leodrive_gatevcu_adapter/vcu_sender.hpp
+++ leodrive_gatevcu_adapter/include/leodrive_gatevcu_adapter/vcu_sender.hpp
@@ -2,6 +2,7 @@
 #include "rclcpp/rclcpp.hpp"
 
 #include "can_msgs/msg/frame.hpp"
+#include "leodrive_gatevcu_msgs/msg/longitudinal.hpp"
 #include "leodrive_gatevcu_msgs/msg/steering_wheel.hpp"
 
 namespace leodrive_gatevcu_adapter
@@ -9,6 +10,7 @@ namespace leodrive_gatevcu_adapter
 
 using CanFrame = can_msgs::msg::Frame;
 using SteeringMsg = leodrive_gatevcu_msgs::msg::SteeringWheel;
+using LongitudinalMsg = leodrive_gatevcu_msgs::msg::Longitudinal;
 
 constexpr std::string_view frame_id{"can"};
 
@@ -17,10 +19,12 @@ class VcuSender : public rclcpp::Node
 public:
   explicit VcuSender(const rclcpp::NodeOptions & options);
   void steering_callback(const SteeringMsg & msg);
+  void longitudinal_callback(const LongitudinalMsg & msg);
 
 private:
   rclcpp::Publisher<CanFrame>::SharedPtr can_frame_pub_;
   rclcpp::Subscription<SteeringMsg>::SharedPtr steering_sub_;
+  rclcpp::Subscription<LongitudinalMsg>::SharedPtr longitudinal_sub_;
 };
 
 }  // namespace leodrive_gatevcu_adapter
--- leodrive_gatevcu_adapter/src/vcu_sender.cpp
+++ leodrive_gatevcu_adapter/src/vcu_sender.cpp
@@ -11,6 +11,10 @@ VcuSender::VcuSender(const rclcpp::NodeOptions & options) : Node{"vcu_sender", o
   steering_sub_ = create_subscription<SteeringMsg>(
     "steering_wheel", rclcpp::SensorDataQoS(),
     std::bind(&VcuSender::steering_callback, this, std::placeholders::_1));
+
+  longitudinal_sub_ = create_subscription<LongitudinalMsg>(
+    "longitudinal", rclcpp::SensorDataQoS(),
+    std::bind(&VcuSender::longitudinal_callback, this, std::placeholders::_1));
 }
 
 void VcuSender::steering_callback(const SteeringMsg & msg)
@@ -26,6 +30,25 @@ void VcuSender::steering_callback(const SteeringMsg & msg)
   cmds.set_steering_wheel_torque = msg.torque;
 
   can_frame.id = Pack_FrontWheelCommands_drivedb(
+    &cmds, can_frame.data.data(), &can_frame.dlc,
+    reinterpret_cast<uint8_t *>(&can_frame.is_extended));
+
+  can_frame_pub_->publish(can_frame);
+}
+
+void VcuSender::longitudinal_callback(const LongitudinalMsg & msg)
+{
+  CanFrame can_frame{};
+  can_frame.header.frame_id = frame_id;
+  can_frame.header.stamp = now();
+  can_frame.is_rtr = false;
+  can_frame.is_error = false;
+
+  LongitudinalCommandsV2_t cmds{};
+  cmds.set_gas_pedal_pos_ro = msg.gas_pedal;
+  cmds.set_brake_force_ro = msg.brake_pedal;
+
+  can_frame.id = Pack_LongitudinalCommandsV2_drivedb(
     &cmds, can_frame.data.data(), &can_frame.dlc,
     reinterpret_cast<uint8_t *>(&can_frame.is_extended));
 
