94b807d7bb4b806886d619c76e04b7aaab93aabe
Mehmet Emin BAŞOĞLU <mehmeteminbasoglu@hotmail.com>
2024-04-04 14:19:11 +0300

    refactor: tidy up

--- leodrive_gatevcu_joy/src/button.cpp
+++ leodrive_gatevcu_joy/src/button.cpp
@@ -28,43 +28,21 @@ void Button::update_input(const bool & is_pressed)
 
 void Button::tick()
 {
-  switch (current_press_state_) {
-    case PressState::PRESSING: {
-      // Check hold time
-      const auto delta_time = node_.now() - *press_time_;
-      if (delta_time > hold_duration) {
-        current_button_state_ = ButtonState::HOLD;  // onHold()
-        current_press_change_state_ = PressChangeState::IDLE;
-        // current_press_state_ = PressState::NOT_PRESSED;
-      }
-      break;
+  if (current_press_state_ == PressState::PRESSING) {
+    const auto delta_time = node_.now() - *press_time_;
+    if (delta_time > hold_duration) {
+      current_button_state_ = ButtonState::HOLD;
+      current_press_change_state_ = PressChangeState::IDLE;
     }
-    case PressState::NOT_PRESSED:
-      // RCLCPP_INFO_STREAM(node_.get_logger(), "        Not pressed");
-      break;
   }
 
-  switch (current_press_change_state_) {
-    case PressChangeState::PRESSED:
-      // RCLCPP_INFO_STREAM(node_.get_logger(), "        Pressed");
-      break;
-    case PressChangeState::RELEASED:
-      RCLCPP_INFO_STREAM(node_.get_logger(), "        Released");
-      if (current_button_state_ != ButtonState::HOLD) {
-        RCLCPP_INFO_STREAM(
-          node_.get_logger(),
-          "current_button_state_: "
-            << static_cast<std::underlying_type<ButtonState>::type>(current_button_state_));
-        current_button_state_ = ButtonState::CLICK;
-        break;
-      }
+  if (current_press_change_state_ == PressChangeState::RELEASED) {
+    if (current_button_state_ != ButtonState::HOLD) {
+      current_button_state_ = ButtonState::CLICK;
+    } else {
       current_press_change_state_ = PressChangeState::IDLE;
       current_button_state_ = ButtonState::IDLE;
-
-      break;
-    case PressChangeState::IDLE:
-      // RCLCPP_INFO_STREAM(node_.get_logger(), "        IDLEEEE");
-      break;
+    }
   }
 
   switch (current_button_state_) {
@@ -78,7 +56,6 @@ void Button::tick()
       RCLCPP_INFO_STREAM(node_.get_logger(), "  ***** Hold");
       break;
     case ButtonState::IDLE:
-
       break;
   }
 }
